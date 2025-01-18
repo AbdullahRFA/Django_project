c5428a7fe3f4bc489068f17f57bbddfe31eac755
AbdullahRFA <shakibrybmn@gmail.com>
2025-01-19 02:19:43 +0600

    modify contact.html modify index.html

--- hello/home/views.py
+++ hello/home/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is home page")
+    context={
+        'variable' : 'this is sent',
+        'name':'Abdullah Nazmus-Sakib'
+    }
+    # return HttpResponse("This is home page")
+    return render(request, 'index.html',context)
 
 def about(request):
-    return HttpResponse("This is about page")
+    # return HttpResponse("This is about page")
+    return render(request,"about.html")
 def services(request):
-    return HttpResponse("This is services page")
+    # return HttpResponse("This is services page")
+    return render(request,"services.html")
 def contact(request):
-    return HttpResponse("This is contact page")
+    # return HttpResponse("This is contact page")
+    return render(request,"contact.html")
